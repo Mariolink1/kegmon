a9c01cea82e9879e7c7679425a46b0a1a0fda991
Chase Matthews <cfm5480@psu.edu>
2022-04-08 21:36:28 -0400

    Kegs were backwards lmao

--- kegmon.py
+++ kegmon.py
@@ -13,8 +13,8 @@ import os.path
 #endregion
 #region GPIO + Flowmeter Library Configuration
 
-GPIO_tap0 = 4
-GPIO_tap1 = 3
+GPIO_tap0 = 3
+GPIO_tap1 = 4
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(GPIO_tap0, GPIO.IN, pull_up_down=GPIO.PUD_UP)
